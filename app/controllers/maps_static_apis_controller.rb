0f422592e9e3c5ce252e052748a0b8099de0d57f
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-11-21 15:03:10 +0700

    init_map_static_api

--- app/controllers/maps_static_apis_controller.rb
+++ app/controllers/maps_static_apis_controller.rb
@@ -1,4 +1,53 @@
 class MapsStaticApisController < ApplicationController
+  helper_method :center, :zoom, :width, :height, :formats, :format, :maptypes, :maptype
+
   def index
+    @params = {
+      key: ENV["GMP_API_KEY"],
+      center: center,
+      zoom: zoom,
+      size: size,
+      maptype: maptype
+    }
+
+    @src = "#{Settings.staticmap.base_url}?#{@params.to_query}"
+  end
+
+  private
+
+  def center
+    params[:center].presence || Settings.staticmap.center
+  end
+
+  def zoom
+    params[:zoom].presence || Settings.staticmap.zoom
+  end
+
+  def width
+    params[:width].presence || Settings.staticmap.width
+  end
+
+  def height
+    params[:height].presence || Settings.staticmap.height
+  end
+
+  def size
+    "#{width}x#{height}"
+  end
+
+  def formats
+    Settings.staticmap.formats.map {|format| format.split(";")}
+  end
+
+  def format
+    params[:format].presence || Settings.staticmap.format
+  end
+
+  def maptypes
+    Settings.staticmap.maptypes.map {|format| format.split(";")}
+  end
+
+  def maptype
+    params[:maptype].presence || Settings.staticmap.maptype
   end
 end
